8a0cea351c238961d77ac445cc1ab0943154f67e
Tomoatsu Shimada <shimada@walbrix.com>
2017-08-18 10:39:39 +0900

    save to db functionality added

--- poll.py
+++ poll.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python
 import time,datetime,sys
+import MySQLdb
 from pyepsolartracer.client import EPsolarTracerClient
 from pyepsolartracer.registers import registers,coils
-
-client = EPsolarTracerClient(port = "/dev/ttyXRUSB0")
-client.connect()
-
-response = client.read_device_info()
-print "# Manufacturer:", repr(response.information[0])
-print "# Model:", repr(response.information[1])
-print "# Version:", repr(response.information[2])
 
 registers = [
     "Charging equipment input voltage",
@@ -19,15 +12,35 @@ registers = [
     "Charging equipment output current",
     "Discharging equipment output voltage",
     "Battery Temperature",
-    "Generated energy today"
+    "Generated energy today",
+    "Discharging equipment output power"
 ]
+
+client = EPsolarTracerClient(port = "/dev/ttyXRUSB0")
+client.connect()
+
+response = client.read_device_info()
+print "# Manufacturer:", repr(response.information[0])
+print "# Model:", repr(response.information[1])
+print "# Version:", repr(response.information[2])
+
+conn = MySQLdb.connect(db="solar")
+conn.autocommit(True)
+
+cur = conn.cursor()
 
 try:
     while True:
         values = [ client.read_input(x).value for x in registers ]
-        print "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f" % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7])
+        now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    	cur.execute("replace into data(t,piv,pia,piw,pov,poa,bv,temp,kwh) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (now_str, values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7]))
+
+        print "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f" % (now_str, values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8])
         sys.stdout.flush()
         time.sleep(5)
 finally:
+    cur.close()
+    conn.close()
     client.close()
     print "# End of file"
