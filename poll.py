30c37ffef608cc3813b83f374638a0545cb131db
Tomoatsu Shimada <shimada@walbrix.com>
2017-09-19 11:43:53 +0900

    data record reduction

--- poll.py
+++ poll.py
@@ -33,12 +33,13 @@ try:
     while True:
         values = [ client.read_input(x).value for x in registers ]
         now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        piv = values[0]
 
-    	cur.execute("replace into data(t,piv,pia,piw,pov,poa,bv,temp,kwh) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (now_str, values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7]))
+    	cur.execute("replace into data(t,piv,pia,piw,pov,poa,bv,temp,kwh) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (now_str, piv,values[1],values[2],values[3],values[4],values[5],values[6],values[7]))
 
-        print "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f" % (now_str, values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8])
+        print "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f" % (now_str, piv,values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8])
         sys.stdout.flush()
-        time.sleep(5)
+        time.sleep(5 if piv > 0.00 else 60)
 finally:
     cur.close()
     conn.close()
