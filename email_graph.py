9637a13604cd0d1cb9194da17793680e19e25dc8
Tomoatsu Shimada <shimada@walbrix.com>
2017-10-31 15:46:08 +0900

    ability to specify nodename

--- email_graph.py
+++ email_graph.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse,smtplib,datetime
+import argparse,smtplib,datetime,socket
 import email.mime.multipart,email.mime.text,email.mime.application,email.header
 
 import graph
@@ -11,19 +11,19 @@ if __name__ == '__main__':
     parser.add_argument("gmail_addr", type = str)
     parser.add_argument("gmail_password", type = str)
     parser.add_argument("recipient_addr", type = str)
+    parser.add_argument("-n", "--nodename", type = str, default = socket.gethostname())
     parser.add_argument("-d", "--date", type = str, dest = "date", default = None)
     args = parser.parse_args()
-    
-    hostname = 'motion'
+    nodename = args.nodename
 
     date = args.date if args.date is not None else (datetime.date.today() - datetime.timedelta(1)).strftime("%Y-%m-%d")
 
     msg = email.mime.multipart.MIMEMultipart()
     msg["From"] = args.gmail_addr
     msg["To"] = args.recipient_addr
-    msg["Subject"] = email.header.Header(u"%s のグラフ" % date, "utf-8")
+    msg["Subject"] = email.header.Header(u"%s %s のグラフ" % (nodename,date), "utf-8")
     msg.attach(email.mime.text.MIMEText("グラフ"))
-    imgpart = email.mime.application.MIMEApplication(graph.generate_graph(hostname,date), "image/png")
+    imgpart = email.mime.application.MIMEApplication(graph.generate_graph(nodename,date), "image/png")
     imgpart["Content-Disposition"] = 'attachment; filename="graph%s.png"' % date
     msg.attach(imgpart)
 
