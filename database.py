0c37567867916bd89c0da2aaf940653a2415fd94
Tomoatsu Shimada <shimada@walbrix.com>
2017-11-03 19:29:47 +0900

    ability to change nodename

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 import MySQLdb
 
 class Connection:
-    def __init__(self, dbname = "solar", autocommit = True):
+    def __init__(self, dbhost = "localhost", dbname = "solar", autocommit = True):
+        self.dbhost = dbhost
         self.dbname = dbname
         self.autocommit = autocommit
 
     def __enter__(self):
-        self.conn = MySQLdb.connect(db=self.dbname)
+        self.conn = MySQLdb.connect(host=self.dbhost,db=self.dbname)
         self.conn.autocommit(self.autocommit)
         self.cursor = self.conn.cursor()
         return self.cursor
--- poll.py
+++ poll.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-import time,datetime,sys
+import time,datetime,sys,socket,argparse
 import MySQLdb
 from pyepsolartracer.client import EPsolarTracerClient
 from pyepsolartracer.registers import registers,coils
+import database
 
 registers = [
     "Charging equipment input voltage",
@@ -16,34 +17,35 @@ registers = [
     "Consumed energy today"
 ]
 
-client = EPsolarTracerClient(port = "/dev/ttyXRUSB0")
-client.connect()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--nodename", type = str, dest = "nodename", default=socket.gethostname())
+    parser.add_argument("-H", "--dbhost", type = str, dest = "dbhost", default="localhost")
+    parser.add_argument("-p", "--port", type = str, dest = "port", default = "/dev/ttyXRUSB0")
+    args = parser.parse_args()
 
-response = client.read_device_info()
-print "# Manufacturer:", repr(response.information[0])
-print "# Model:", repr(response.information[1])
-print "# Version:", repr(response.information[2])
+    print "# Nodename:", args.nodename
 
-hostname = 'motion'
+    client = EPsolarTracerClient(port = args.port)
+    client.connect()
 
-conn = MySQLdb.connect(db="solar")
-conn.autocommit(True)
+    response = client.read_device_info()
+    print "# Manufacturer:", repr(response.information[0])
+    print "# Model:", repr(response.information[1])
+    print "# Version:", repr(response.information[2])
 
-cur = conn.cursor()
+    try:
+        with database.Connection(dbhost=args.dbhost) as cur:
+            while True:
+                values = [ client.read_input(x).value for x in registers ]
+                now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                piv = values[0]
 
-try:
-    while True:
-        values = [ client.read_input(x).value for x in registers ]
-        now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        piv = values[0]
+            	cur.execute("replace into data(hostname,t,piv,pia,piw,pov,poa,loadw,temp,kwh,lkwh) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (args.nodename,now_str, piv,values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8]))
 
-    	cur.execute("replace into data(hostname,t,piv,pia,piw,pov,poa,loadw,temp,kwh,lkwh) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (hostname,now_str, piv,values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8]))
-
-        print "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f" % (now_str, piv,values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8])
-        sys.stdout.flush()
-        time.sleep(5 if piv > 0.00 else 60)
-finally:
-    cur.close()
-    conn.close()
-    client.close()
-    print "# End of file"
+                print "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f" % (now_str, piv,values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8])
+                sys.stdout.flush()
+                time.sleep(5 if piv > 0.00 else 60)
+    finally:
+        client.close()
+        print "# End of file"
