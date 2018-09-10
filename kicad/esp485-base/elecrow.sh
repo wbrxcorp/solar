1fcb5e6faddedd76596e81a211a15c1121972778
Tomoatsu Shimada <shimada@walbrix.com>
2018-09-10 17:08:20 +0900

    improve elecrow script

--- kicad/esp485-base/elecrow.sh
+++ kicad/esp485-base/elecrow.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+DIR=`dirname $0`
+ABS_DIR=`readlink -f $DIR`
+PROJ_NAME=`basename $ABS_DIR`
 rm -rf elecrow
 mkdir elecrow
-cp *.gtl elecrow/elecrow.gtl
-cp *.gbl elecrow/elecrow.gbl
-cp *.gto elecrow/elecrow.gto
-cp *.gbo elecrow/elecrow.gbo
-cp *.gts elecrow/elecrow.gts
-cp *.gbs elecrow/elecrow.gbs
-cp *.gm1 elecrow/elecrow.gml
-cp *.drl elecrow/elecrow.txt
-cd elecrow && zip ../elecrow.zip * && cd .. && rm -rf elecrow
+cp *.gtl elecrow/$PROJ_NAME.gtl
+cp *.gbl elecrow/$PROJ_NAME.gbl
+cp *.gto elecrow/$PROJ_NAME.gto
+cp *.gbo elecrow/$PROJ_NAME.gbo
+cp *.gts elecrow/$PROJ_NAME.gts
+cp *.gbs elecrow/$PROJ_NAME.gbs
+cp *.gm1 elecrow/$PROJ_NAME.gml
+cp *.drl elecrow/$PROJ_NAMEw.txt
+cd elecrow && zip ../$PROJ_NAME.zip * && cd .. && rm -rf elecrow
