e24698e172d42de2ef0062792a1a07586f2a18dd
Tomoatsu Shimada <shimada@walbrix.com>
2018-11-30 07:42:37 +0900

    fix typo

--- kicad/rj45/elecrow.sh
+++ kicad/rj45/elecrow.sh
@@ -11,5 +11,5 @@ cp *.gbo elecrow/$PROJ_NAME.gbo
 cp *.gts elecrow/$PROJ_NAME.gts
 cp *.gbs elecrow/$PROJ_NAME.gbs
 cp *.gm1 elecrow/$PROJ_NAME.gml
-cp *.drl elecrow/$PROJ_NAMEw.txt
+cp *.drl elecrow/$PROJ_NAME.txt
 cd elecrow && zip ../$PROJ_NAME.zip * && cd .. && rm -rf elecrow
