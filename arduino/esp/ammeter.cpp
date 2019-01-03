0352909568a138262dec3197e7ef384702947c95
Tomoatsu Shimada <shimada@walbrix.com>
2019-01-03 14:45:20 +0900

    check existence of INA219 before begin

--- arduino/esp/ammeter.cpp
+++ arduino/esp/ammeter.cpp
@@ -2,26 +2,40 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 
-static Adafruit_INA219 ina219;
+static Adafruit_INA219 ina219(INA219_ADDRESS);
+static bool ina219_started = false;
+
+static bool start_ina219()
+{
+  Wire.beginTransmission(INA219_ADDRESS);
+  if (Wire.endTransmission() != 0) {
+    Serial.println("INA219 at address 0x40 not found!");
+    return false;
+  }
+  // else
+  ina219.begin();
+  Serial.println("INA219 started.");
+  return true;
+}
 
 void setup_ammeter()
 {
-  ina219.begin();
+  Wire.begin();
+  ina219_started = start_ina219();
 }
 
 void loop_ammeter()
 {
-    float shuntvoltage = 0;
-    float busvoltage = 0;
-    float current_mA = 0;
-    float loadvoltage = 0;
-    float power_mW = 0;
+  if (!ina219_started) {
+    ina219_started = start_ina219();
+  }
 
-    shuntvoltage = ina219.getShuntVoltage_mV();
-    busvoltage = ina219.getBusVoltage_V();
-    current_mA = ina219.getCurrent_mA();
-    power_mW = ina219.getPower_mW();
-    loadvoltage = busvoltage + (shuntvoltage / 1000);
+  if (ina219_statred) {
+    float shuntvoltage = ina219.getShuntVoltage_mV();
+    float busvoltage = ina219.getBusVoltage_V();
+    float current_mA = ina219.getCurrent_mA();
+    float power_mW = ina219.getPower_mW();
+    float loadvoltage = busvoltage + (shuntvoltage / 1000);
 
     Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
     Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
@@ -29,6 +43,7 @@ void loop_ammeter()
     Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
     Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
     Serial.println("");
+  }
 
-    delay(2000);
+  delay(2000);
 }
