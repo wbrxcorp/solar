19361d91ff8393fac54ae63ac083bae8ffc34eb0
Tomoatsu Shimada <shimada@walbrix.com>
2019-03-15 23:56:02 +0900

    add led operation to thermometer

--- arduino/esp/thermometer.cpp
+++ arduino/esp/thermometer.cpp
@@ -3,6 +3,10 @@
 #elif defined(ARDUINO_ARCH_ESP32)
 #include <WiFi.h>
 #include <SPIFFS.h>
+#include <esp32-hal-ledc.h>
+static TaskHandle_t ledTask;
+static SemaphoreHandle_t mutex = NULL;
+static uint16_t eCO2_shared = 0;
 #endif
 #include <FS.h>
 
@@ -15,6 +19,10 @@ static unsigned long last_update = 0;
 static bool ccs811_present = false;
 
 static const uint16_t interval = 1000;
+static const uint8_t red_pin = 33;
+static const uint8_t yellow_pin = 25;
+static const uint8_t green_pin = 26;
+
 
 void early_setup_thermometer()
 {
@@ -53,6 +61,60 @@ void load_background_image()
   SPIFFS.end();
 }
 
+static uint8_t get_duty(double ratio){
+  return (uint8_t)round(exp(log(255.0) - (1 - ratio) * log(255.0/*C*/))) - 1;
+}
+
+#ifdef ARDUINO_ARCH_ESP32
+static void led(void *pvParams)
+{
+  int32_t green, yellow, red;
+  while (true) {
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    uint16_t eCO2 = eCO2_shared;
+    xSemaphoreGive(mutex);
+
+    if (eCO2 == 0) {
+      green = yellow = red = 255;
+    } else {
+      unsigned long t = millis();
+      green = (300 - (eCO2 - 700)) * 255 / 300;
+      if (green < 0) green = 0;
+      if (green > 255) { // blink
+        if (t / 1000 % 2) { // on
+          green = 255;
+        } else { // off
+          green = (1000 - t % 1000) * 255 / 1000;
+        }
+      }
+
+      yellow = eCO2 >= 1000? ((1500 - eCO2) * 255 / 500) : ((eCO2 - 700) * 255 / 300);
+      if (yellow < 0) yellow = 0;
+      if (yellow > 255) yellow = 255;
+
+      red = (eCO2 - 1000) * 255 / 500;
+      if (red < 0) red = 0;
+      if (red > 255) { // blink
+        int interval;
+        if (eCO2 >= 3000) interval = 250;
+        else if (eCO2 >= 2000) interval = 500;
+        else interval = 1000;
+
+        if (t / interval % 2) {
+          red = 255;
+        } else {
+          red = (interval - t % interval) * 255 / interval;
+        }
+      }
+    }
+
+    ledcWrite(0, get_duty(red / 255.0));
+    ledcWrite(1, get_duty(yellow / 255.0));
+    ledcWrite(2, get_duty(green / 255.0));
+    delay(10);
+  }
+}
+#endif
 
 void setup_thermometer()
 {
@@ -79,6 +141,17 @@ void setup_thermometer()
     while(!ccs.available());
     float temp = ccs.calculateTemperature();
     ccs.setTempOffset(temp - 25.0);
+#ifdef ARDUINO_ARCH_ESP32
+    ledcSetup(0, 5000, 8);
+    ledcAttachPin(red_pin, 0);
+    ledcSetup(1, 5000, 8);
+    ledcAttachPin(yellow_pin, 1);
+    ledcSetup(2, 5000, 8);
+    ledcAttachPin(green_pin, 2);
+    mutex = xSemaphoreCreateMutex();
+    xTaskCreatePinnedToCore(led, "led", 4096, NULL, 5, &ledTask, 0);
+#endif
+
   } else {
     Serial.println("Could not find a CCS811 sensor.");
   }
@@ -142,9 +215,7 @@ void loop_thermometer()
 {
   unsigned long current_time = millis();
   if (current_time < last_update + interval) return;
-
   // else
-
   float temperature = bme.readTemperature();
   float humidity =  bme.readHumidity();
   thermometer_print_values(temperature, humidity, (uint16_t)(bme.readPressure() / 100.0F));
@@ -154,7 +225,13 @@ void loop_thermometer()
     if (ccs.available()) {
       float temp = ccs.calculateTemperature();
       if(!ccs.readData()){
-        gasmeter_print_values((uint16_t)ccs.geteCO2(), ccs.getTVOC(), temp);
+        uint16_t eCO2 = ccs.geteCO2();
+#ifdef ARDUINO_ARCH_ESP32
+        xSemaphoreTake(mutex, portMAX_DELAY);
+        eCO2_shared = eCO2;
+        xSemaphoreGive(mutex);
+#endif
+        gasmeter_print_values(eCO2, ccs.getTVOC(), temp);
       }
     }
   }
