6ee9667b8da52a95565d7bb8eb47132a2bc7afc8
Tomoatsu Shimada <shimada@walbrix.com>
2019-05-01 06:15:13 +0900

    Update import.py

--- import.py
+++ import.py
@@ -14,6 +14,7 @@ create table data(
 	kwh float,
 	lkwh float
 );
+create index t_idx using btree on data(t);
 
 alter table data add column hostname varchar(32) not null default 'motion' FIRST;
 alter table data drop primary key,add primary key(hostname,t), change column hostname hostname varchar(32) not null;
